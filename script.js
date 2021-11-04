acd9de62012798d72752b9cee9d4509d66f5dd9a
Artyom <sleva.sverxy@mail.ru>
2021-11-05 00:24:01 +0400

    added reset button and resize slider

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 'use strict';
 const sketchpad = document.querySelector('#sketchpad');
+const resetBtn = document.querySelector('#reset');
+const sketchpadSizeRange = document.querySelector('#size');
 
-createGrid(16);
+createGrid();
+setColorChangeListener();
 
-const gridCells = document.querySelectorAll('.cell');
+sketchpadSizeRange.addEventListener('input', createGrid);
+resetBtn.addEventListener('click', resetSketch);
 
-gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor));
+function createGrid() {
+  while(sketchpad.firstChild) {
+    sketchpad.removeChild(sketchpad.firstChild);
+  }
 
-function createGrid(size) {
+  let size = sketchpadSizeRange.value;
+  console.log(size);
   for(let i=0; i < size; i++) {
     let row = document.createElement('div');
     row.setAttribute('class', `row`);
@@ -15,13 +23,24 @@ function createGrid(size) {
     for(let j=0; j < size; j++) {
       let div = document.createElement('div');
       div.setAttribute('class', 'cell');
-      // div.textContent = `${i}x${j}`;
       row.appendChild(div)
     }
     sketchpad.appendChild(row);
   }
+
+  setColorChangeListener();
 }
 
 function changeColor(e) {
   this.classList.add('black-colored');
+}
+
+function resetSketch() {
+  const gridCells = document.querySelectorAll('.cell');
+  gridCells.forEach(cell => cell.setAttribute('class', 'cell'))
+}
+
+function setColorChangeListener() {
+  const gridCells = document.querySelectorAll('.cell');
+  gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor));
 }
